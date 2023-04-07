b2f01451194f94018d86815a72293aab12cb153e
AlexiosIko <67430469+AlexiosIko@users.noreply.github.com>
2023-04-06 23:24:05 -0700

    Add files via upload

--- db/connection.js
+++ db/connection.js
@@ -1,5 +1,9 @@
 require('dotenv').config(); // This for my enirovment variable
 
 const mongoose = require("mongoose");
-let mongoDB = process.env.DB_CONNECTION;
+
+
+// let mongoDB = process.env.DB_CONNECTION; // GITHUB HIDES MY .ENV FILE SO IT DOESN'T WORK
+let mongoDB = 'mongodb+srv://Alexi:verysecurepassword@cluster0.yjwaf2u.mongodb.net/google-maps-project';
+
 module.exports = mongoose.connect(mongoDB);
